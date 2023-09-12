ed957c546c13eca9e72ddf1bce4f29ddb3c8c904
alakhkaler <alakhkaler.ak@gmail.com>
2023-09-12 14:28:17 +0530

    nn

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Other build configuration options
+    outDir: 'build', // Update to 'build'
+  },
 })
